bbacebe05f1109c1db4b3f7de23f8246a2dfbe1e
UroojAslam786 <129731884+UroojAslam786@users.noreply.github.com>
2023-05-17 11:17:28 +0500

    assigbment 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from "react";
+import { useQuery } from 'react-query';
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState("");
+  
+  const fetchData = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=250d4ebd906279665f1ad04f46b48a73&units=metric`;
+    const res = await fetch(url);
+    return res.json();
+  }
+  
+
+
+      const { data, status, refetch } = useQuery("weatherData", fetchData, { enabled: false });
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+      
+        <input type="text" placeholder="Enter City" onChange={(e) => setSearch(e.target.value)}/>
+        <button type='button' className="styling" onClick={refetch}>Search</button>
+      </div>
+      <div className="App">
+        {status === "error" && <p>Error for Fetching</p>}
+        {status === "loading" && <p>Fetching data</p>}
+        {status === "success" && data && (
+  <div>
+    <p>City: {data.name}</p>
+    <p>Temperature: {data.main.temp}</p>
+    <p>{data.weather[0]?.main}</p>
+  </div>
+
+)}
+
+</div>
+    </>
   );
-}
+};
 
 export default App;
